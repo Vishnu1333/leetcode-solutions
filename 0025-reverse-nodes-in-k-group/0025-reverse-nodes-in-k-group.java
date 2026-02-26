7b8f1d8e3730ccdfe68f339bfeee4e47b840660f
Vishnu vardhan <127837652+Vishnu1333@users.noreply.github.com>
2026-02-26 10:10:22 +0530

    Time: 0 ms (100%), Space: 46.8 MB (15.22%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode current = head;
+        int count = 0;
+
+        while (current != null && count < k) {
+            current = current.next;
+            count++;
+        }
+
+        if (count == k) {
+            current = reverseKGroup(current, k);
+            
+            while (count-- > 0) {
+                ListNode temp = head.next;
+                head.next = current;
+                current = head;
+                head = temp;
+            }
+            head = current;
+        }
+
+        return head;
+    }
+}
